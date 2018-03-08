a1086e59425dace386480183ce6e17d26e939792
feng <dangerousfeng@gmail.com>
2018-03-08 17:21:35 +0800

    选择视频，载入视频

--- app/src/main/java/com/feng/opencourse/CreateSectionActivity.java
+++ app/src/main/java/com/feng/opencourse/CreateSectionActivity.java
@@ -8,15 +8,42 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.alibaba.sdk.android.oss.ClientException;
+import com.alibaba.sdk.android.oss.OSS;
+import com.alibaba.sdk.android.oss.OSSClient;
+import com.alibaba.sdk.android.oss.ServiceException;
+import com.alibaba.sdk.android.oss.callback.OSSCompletedCallback;
+import com.alibaba.sdk.android.oss.callback.OSSProgressCallback;
+import com.alibaba.sdk.android.oss.internal.OSSAsyncTask;
+import com.alibaba.sdk.android.oss.model.PutObjectRequest;
+import com.alibaba.sdk.android.oss.model.PutObjectResult;
+import com.feng.opencourse.util.HttpUtil;
+import com.feng.opencourse.util.MyApplication;
 import com.feng.opencourse.util.PermissionsChecker;
+import com.feng.opencourse.util.ProperTies;
+import com.feng.opencourse.util.UriPathUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Properties;
 
 import cn.jzvd.JZVideoPlayerStandard;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static com.feng.opencourse.util.HttpUtil.getRespData;
 
 public class CreateSectionActivity extends AppCompatActivity {
 
@@ -25,8 +52,13 @@ public class CreateSectionActivity extends AppCompatActivity {
     private EditText etSectionName;
     private EditText etSectionDesc;
     private Button btnCreateSec;
+    private ProgressBar pbUploadSec;
 
     private String secVideoPath;
+    private String courseId;
+    private String secId;
+
+    private MyApplication myapp;
 
     private final int VIDEO_REQUEST_CODE = 3;
     private static final int PERMISSIONS_REQUEST_CODE = 2;
@@ -42,6 +74,9 @@ public class CreateSectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_section);
 
+        Intent intent = getIntent();
+        courseId = intent.getStringExtra("courseId");
+        myapp = (MyApplication) getApplication();
         mPermissionsChecker = new PermissionsChecker(this);
         find();
     }
@@ -54,6 +89,7 @@ public class CreateSectionActivity extends AppCompatActivity {
         etSectionDesc = (EditText) findViewById(R.id.et_sectionDesc);
         btnCreateSec = (Button) findViewById(R.id.btn_createSection);
         btnCreateSec.setOnClickListener(createSectionListener);
+        pbUploadSec = (ProgressBar) findViewById(R.id.pb_upload_section);
     }
     @Override
     protected void onResume() {
@@ -69,14 +105,97 @@ public class CreateSectionActivity extends AppCompatActivity {
     View.OnClickListener selectSectionListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+            Log.i("print","============btn click=======");
+//            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+            Intent intent=new Intent("android.intent.action.GET_CONTENT");
+            intent.setType("video/*");
             startActivityForResult(intent, VIDEO_REQUEST_CODE);
         }
     };
     Button.OnClickListener createSectionListener = new Button.OnClickListener() {
+
         @Override
         public void onClick(View v) {
+            ivCreateSection.setVisibility(View.GONE);
+            jzVideoPlayerStandard.setClickable(false);
+            jzVideoPlayerStandard.setAlpha(0.5f);
+            pbUploadSec.setVisibility(View.VISIBLE);
 
+            JSONObject json = new JSONObject();
+            try {
+                json.put("ActionId", 201);
+                json.put("JWT", myapp.getJWT());
+                json.put("userId", myapp.getUserId());
+                json.put("courseId",courseId);
+                json.put("secName", etSectionName.getText());
+                json.put("secDesc", etSectionDesc.getText());
+
+                String req = json.toString();
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), req);
+                HttpUtil.sendAsyncRequest(body, new okhttp3.Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Toast.makeText(CreateSectionActivity.this, "创建小节失败", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        try {
+                            String respDataStr = getRespData(response);
+                            JSONObject respDataJson = new JSONObject(respDataStr);
+                            secId = respDataJson.optString("secId");
+                            Properties proper = ProperTies.getProperties(myapp.getApplicationContext());
+                            String endpoint = proper.getProperty("OSS_ENDPOINT");
+                            OSS oss = new OSSClient(
+                                    myapp.getApplicationContext(),
+                                    endpoint,
+                                    myapp.getWriteOnlyOSSCredentialProvider());
+                            PutObjectRequest put = new PutObjectRequest(
+                                    proper.getProperty("OSS_BUCKET_NAME"),
+                                    courseId + "/" +secId ,
+                                    secVideoPath);
+                            // 异步上传时可以设置进度回调
+                            put.setProgressCallback(new OSSProgressCallback<PutObjectRequest>() {
+                                @Override
+                                public void onProgress(PutObjectRequest request, long currentSize, long totalSize) {
+                                    Log.d("PutObject", "currentSize: " + currentSize + " totalSize: " + totalSize);
+                                }
+                            });
+                            OSSAsyncTask task = oss.asyncPutObject(put, new OSSCompletedCallback<PutObjectRequest, PutObjectResult>() {
+                                @Override
+                                public void onSuccess(PutObjectRequest request, PutObjectResult result) {
+                                    Log.d("PutObject", "UploadSuccess");
+                                    Log.d("ETag", result.getETag());
+                                    Log.d("RequestId", result.getRequestId());
+                                }
+                                @Override
+                                public void onFailure(PutObjectRequest request, ClientException clientExcepion, ServiceException serviceException) {
+                                    // 请求异常
+                                    if (clientExcepion != null) {
+                                        // 本地异常如网络异常等
+                                        clientExcepion.printStackTrace();
+                                    }
+                                    if (serviceException != null) {
+                                        // 服务异常
+                                        Log.e("ErrorCode", serviceException.getErrorCode());
+                                        Log.e("RequestId", serviceException.getRequestId());
+                                        Log.e("HostId", serviceException.getHostId());
+                                        Log.e("RawMessage", serviceException.getRawMessage());
+                                    }
+                                }
+                            });
+                            task.waitUntilFinished(); // 可以等待任务完成
+                            System.out.println("===finish()====");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        //String responseData = response.body().string();
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     };
     @Override
@@ -86,22 +205,18 @@ public class CreateSectionActivity extends AppCompatActivity {
             case VIDEO_REQUEST_CODE:
                 if ( resultCode == RESULT_OK && null != data) {
                     Uri selectedVideo = data.getData();
-                    String[] filePathColumn = {MediaStore.Video.Media.DATA};
-
-                    Cursor cursor = getContentResolver().query(selectedVideo,
-                            filePathColumn, null, null, null);
-                    cursor.moveToFirst();
-
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    secVideoPath = cursor.getString(columnIndex);
-                    cursor.close();
-
+                    secVideoPath = UriPathUtil.getPath(myapp.getApplicationContext(),selectedVideo);
                     jzVideoPlayerStandard.setUp(
                             secVideoPath,
                             JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
                             "");
                     ivCreateSection.setClickable(false);
                     ivCreateSection.setAlpha(0f);
+                    ivCreateSection.setVisibility(View.GONE);
+
+                    jzVideoPlayerStandard.setClickable(true);
+                    jzVideoPlayerStandard.setVisibility(View.VISIBLE);
+
                 }else {
                     Toast.makeText(this, "recChoice", Toast.LENGTH_SHORT).show();
                 }
